5ea842f41b1c20c204007ca746d524723734d9b5
Didier Pereira <krrattoss5@gmail.com>
2026-03-01 16:15:33 -0500

    feat auth middlewares

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js"
 
 const router = Router()
 
 // Admin routes
-router.get("/dashboard", (req, res) => {
+router.get("/", protectedRoute, requireAdmin, (req, res) => {
   res.send("Admin Dashboard")
 })
 
