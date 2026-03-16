ad8bf8af5caeb6acc6c39cb4baaa9cd9694dece4
Didier Pereira <krrattoss5@gmail.com>
2026-03-16 14:20:00 -0500

    feat(frontend): add Zustand store for chat users state

---
+++ frontend/src/stores/useChatStore.tsx
@@ -0,0 +1,27 @@
+import { axiosInstance } from "@/lib/axios"
+import { create } from "zustand"
+
+interface ChatStore {
+  users: any[]
+  fetchUsers: () => Promise<void>
+  isLoading: boolean
+  error: string | null
+}
+
+export const useChatStore = create<ChatStore>((set, get) => ({
+  users: [],
+  isLoading: false,
+  error: null,
+  fetchUsers: async () => {
+    set({ isLoading: true, error: null })
+
+    try {
+      const res = await axiosInstance.get("/users")
+      set({ users: res?.data })
+    } catch (error: any) {
+      set({ error: error.response.data.message })
+    } finally {
+      set({ isLoading: false })
+    }
+  },
+}))
