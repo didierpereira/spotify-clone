57ede10b09578083e9ede3fc0ccdfd3020362a6e
Didier Pereira <krrattoss5@gmail.com>
2026-03-19 23:00:02 -0500

    feat: add Song routes and public endpoints

--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,9 +1,19 @@
-import { Router } from "express";
+import { Router } from "express"
+import {
+  getAllSongs,
+  getSongById,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js"
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js"
 
 const router = Router()
 
-router.get("/", (req, res) => {
-  res.send("User route")
-})
+router.get("/", protectedRoute, requireAdmin, getAllSongs)
+router.get("/:id", protectedRoute, requireAdmin, getSongById)
+router.get("/featured", getFeaturedSongs)
+router.get("/made-for-you", getMadeForYouSongs)
+router.get("/trending", getTrendingSongs)
 
 export default router
