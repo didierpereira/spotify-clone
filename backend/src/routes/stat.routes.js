a53ca0809931f31366dddd5c9cc144a56fbb2380
Didier Pereira <krrattoss5@gmail.com>
2026-03-19 23:01:22 -0500

    feat: add stat route protected for admin

---
+++ backend/src/routes/stat.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import { getStats } from "../controllers/stat.controller.js"
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js"
+
+const router = Router()
+
+router.get("/", protectedRoute, requireAdmin, getStats)
+
+export default router
