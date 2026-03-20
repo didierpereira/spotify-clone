df227a5b4ddc2893cf2059293f7bc1eec6c1c59f
Didier Pereira <krrattoss5@gmail.com>
2026-03-19 22:59:20 -0500

    feat: add Song controller endpoints (list/get/featured/made/trending)

---
+++ backend/src/controllers/song.controller.js
@@ -0,0 +1,94 @@
+import Song from "../models/song.model.js"
+
+export const getAllSongs = async (req, res, next) => {
+  try {
+    // -1 = Descending  => newest -> oldest
+    const songs = await Song.find().sort({ createdAt: -1 })
+    res.status(200).json(songs)
+  } catch (error) {
+    console.error("Error fetching songs:", error)
+    next(error)
+  }
+}
+
+export const getSongById = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const song = await Song.findById(id)
+
+    if (!song) {
+      return res.status(404).json({ message: "Song not found" })
+    }
+
+    res.status(200).json(song)
+  } catch (error) {
+    console.error("Error fetching song:", error)
+    next(error)
+  }
+}
+
+export const getFeaturedSongs = async (req, res, next) => {
+  try {
+    const songs = await Song.aggregate([
+      { $sample: { size: 6 } },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ])
+
+    res.status(200).json(songs)
+  } catch (error) {
+    console.error("Error fetching featured songs:", error)
+    next(error)
+  }
+}
+
+export const getMadeForYouSongs = async (req, res, next) => {
+  try {
+    const songs = await Song.aggregate([
+      { $sample: { size: 6 } },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ])
+
+    res.status(200).json(songs)
+  } catch (error) {
+    console.error("Error fetching featured songs:", error)
+    next(error)
+  }
+}
+
+export const getTrendingSongs = async (req, res, next) => {
+  try {
+    const songs = await Song.aggregate([
+      { $sample: { size: 6 } },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ])
+
+    res.status(200).json(songs)
+  } catch (error) {
+    console.error("Error fetching featured songs:", error)
+    next(error)
+  }
+}
