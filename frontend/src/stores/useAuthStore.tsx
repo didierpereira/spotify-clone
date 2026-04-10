c3864b686b36cde28a1dee475be43f8d84539855
Didier Pereira <krrattoss5@gmail.com>
2026-04-10 10:00:00 -0500

    feat(state): add auth and player stores

---
+++ frontend/src/stores/useAuthStore.tsx
@@ -0,0 +1,34 @@
+import { axiosInstance } from "@/lib/axios"
+import { create } from "zustand"
+
+interface AuthStore {
+  isAdmin: boolean
+  isLoading: boolean
+  error: string | null
+
+  checkAdminStatus: () => Promise<void>
+  reset: () => void
+}
+
+export const useAuthStore = create<AuthStore>((set, get) => ({
+  isAdmin: false,
+  isLoading: false,
+  error: null,
+
+  checkAdminStatus: async () => {
+    set({ isLoading: true, error: null })
+
+    try {
+      const res = await axiosInstance.get("/admin/check")
+      set({ isAdmin: res.data.admin })
+    } catch (error: any) {
+      set({ error: error.response.data.message })
+    } finally {
+      set({ isLoading: true })
+    }
+  },
+
+  reset: () => {
+    set({ isAdmin: false, isLoading: false, error: null })
+  },
+}))
