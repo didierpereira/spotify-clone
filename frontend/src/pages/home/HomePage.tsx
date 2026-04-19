cd54f8583854763c3ef304a63514effe31f54754
Didier Pereira <krrattoss5@gmail.com>
2026-04-19 10:00:00 -0500

    feat(home): add HomePage with sections

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,48 @@
 import Topbar from "@/components/topbar/Topbar"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import FeaturedSection from "./components/FeaturedSection"
+import SectionGrid from "./components/SectionGrid"
+import { useMusicStore } from "@/stores/useMusicStore"
+import { useEffect } from "react"
 
 const HomePage = () => {
+  const {
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+
+    isLoading,
+    trendingSongs,
+    madeForYouSongs,
+  } = useMusicStore()
+
+  useEffect(() => {
+    fetchMadeForYouSongs()
+    fetchTrendingSongs()
+  }, [fetchMadeForYouSongs, fetchTrendingSongs])
+
   return (
-    <div>
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
       <Topbar />
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
+          <FeaturedSection />
+
+          <SectionGrid
+            title={"Made for you"}
+            songs={madeForYouSongs}
+            isLoading={isLoading}
+          />
+          <SectionGrid
+            title={"Trending"}
+            songs={trendingSongs}
+            isLoading={isLoading}
+          />
+        </div>
+      </ScrollArea>
+    </main>
   )
 }
 
