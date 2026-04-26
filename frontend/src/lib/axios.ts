e0df0d18e9922e163bdb16c0f050922d46dbce36
Didier Pereira <krrattoss5@gmail.com>
2026-04-26 14:55:42 -0500

    fix:api path

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5000/" : "/",
+    import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
 })
